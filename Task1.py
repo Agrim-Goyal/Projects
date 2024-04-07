9296328cacc08c47b04e196dcafaecef31b50289
Agrim-Goyal <148373285+Agrim-Goyal@users.noreply.github.com>
2024-04-07 08:18:02 +0530

    Update Task1.py, novelties

--- Task1.py
+++ Task1.py
@@ -2,7 +2,7 @@ import numpy as np
 #defining classes
 #agent refers to the object moving from start to stop coordinates with given constraints
 class Agent:
-    def __init__(self, start, stop):
+    def __init__(self, start, stop, constraints):
         self.start = start
         self.stop = stop
 
@@ -24,10 +24,20 @@ class constraint_Node():
         constraint= []
         self.constraint = constraint
         self.solution = solution
-        self.cost= 0    
+        self.cost= 0 
+
+#num_intersections is used to implement Standley's tie breaking 
+def num_intersections(node,solution,index):
+    k=0
+    for i in range(len(solution)):
+        if i==index or node.time>=len(solution[i]):
+            continue
+        if [node.coord]==solution[i][node.time]:
+            k+=1
+    return k
 
 #implementation of A_star
-def A_star(maze, agent,constraints):
+def A_star(maze, agent,constraints,solution,index):
     start = Node(None, agent.start,0)
     open_list = [start]
 #note that I have removed the closed list usually used in A_star as the problem statement mentions that objects cannot wait so in some cases, the best path might be one in which some coordinate needs to be revisited
@@ -35,10 +45,13 @@ def A_star(maze, agent,constraints):
 #calculating the node with least f value       
         current = open_list[0]
         current_index = 0
+#implementation of Standley's tie breaking
+        k=num_intersections(current,solution,index)
         for i in range(1, len(open_list)):
-            if open_list[i].f < current.f:
+            if open_list[i].f < current.f or ((open_list[i].f==current.f) and (k>num_intersections(open_list[i],solution,index))):
                 current = open_list[i]
                 current_index = i
+                k=num_intersections(current,solution,index)
         open_list.pop(current_index)
 #printing result if node achieved is the endpoint
         if current.coord == agent.stop:
@@ -65,13 +78,6 @@ def A_star(maze, agent,constraints):
 #we remove the checking mechanism for a coordinate already existing in the open list because in some case this might lead to an ideal solution not being taken 
             open_list.append(new_node)
 
-#low level search for solutions for each agent depending on currently existing constraints
-def low_level(agents,maze,constraints):
-    a=[]
-    for i in range(len(agents)):
-        a.append(A_star(maze,agents[i],constraints[i]))
-    return a
-
 #cost calculation for solution
 def SIC(solution):
     cost=0
@@ -91,10 +97,9 @@ def validity(solution):
 #high level CBS implementation
 def CBS(maze,agents):
     root=constraint_Node([],None)
-    p=[]
+    root.solution=[]
     for i in range(len(agents)):
-        p.append([])
-    root.solution=low_level(agents,maze,p)
+        root.solution.append(A_star(maze,agents[i],[],root.solution,i))
     root.cost=SIC(root.solution)
 #stores the nodes at the bottom of the tree, we don't store tree because we can get preceding nodes by backtracking
     open=[]
@@ -108,25 +113,25 @@ def CBS(maze,agents):
                 min=open[i].cost
                 x=i
         n=open.pop(x)
-#if valis, we return
+#if valid, we return
         flag=validity(n.solution)
         if flag==0: return n.solution
         conflict=flag
 #we create child nodes with added constraint
         for i in range(2):
-            next_node=constraint_Node(n.solution,n)
+            copy=n.solution.copy()
+            next_node=constraint_Node(copy,n)
             next_node.constraint=[conflict[i],conflict[2],conflict[3]]
             con=next_node
             constraints=[]
-            for j in range(len(agents)):
-                constraints.append([])
 #backtracking to get all constraints
             while con.constraint!=[]:
-                constraint=con.constraint
-                constraints[constraint[0]].append([constraint[1],constraint[2]])
+                if con.constraint[0]==conflict[i]:
+                    constraint=con.constraint
+                    constraints.append([constraint[1],constraint[2]])
                 con=con.parent
 #adding new node with solution to open list
-            next_node.solution=low_level(agents,maze,constraints)
+            next_node.solution[conflict[i]]=A_star(maze,agents[conflict[i]],constraints,next_node.solution,conflict[i])
             open.append(next_node)
         
 #taking user input for maze dimension, obstacles, agents and implementing CBS
@@ -145,6 +150,13 @@ for i in range(n):
     start_x = int(input("Enter starting x coordinate: "))
     stop_y = int(input("Enter stopping y coordinate: "))
     stop_x = int(input("Enter stopping x coordinate: "))
-    agents.append(Agent([start_y,start_x], [stop_y,stop_x]))
+    agents.append(Agent([start_y,start_x], [stop_y,stop_x], []))
 for i in CBS(maze, agents):
         print(i)
+
+
+"""NOVELTIES:
+   1. Implementation of Standley Tie Breaking as given in the research paper
+   2. Storing constraints only one per node so as to improve space complexity and gaining all by backtracking
+   3. Implementing low level search for only the new constraint in each constraint node to improve time complexity
+   4. Removing closed list as due to constraints, a path might involve repeated occurrence of a specific node."""
